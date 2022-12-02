d62fb184d8e0dc153acfe1def966d90646613eb9
Nicolas F <ovdev@fratti.ch>
2022-12-02 02:00:57 +0100

    flask: Switch to tomli/tomllib for TOML parsing
    tomli is an actively maintained project that continues to provide
    what became the Python standard library's toml parser. With Python
    3.11+, we can use Python's included version of tomli, tomllib.
    
    tomli is faster than toml, has full test coverage, and passes those
    tests with flying colours.

--- flask/sunnesiite/__init__.py
+++ flask/sunnesiite/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_caching import Cache
-import toml
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
 
 cache = Cache()
@@ -9,7 +12,12 @@ cache = Cache()
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_file("config.toml", load=toml.load)
+
+    # Can't use from_file with tomllib.load here because it wants it opened in
+    # rb mode for utf-8 and line ending reasons
+    with app.open_instance_resource("config.toml", mode="rb") as cf:
+        conf = tomllib.load(cf)
+    app.config.from_mapping(conf)
 
     from . import main
 
